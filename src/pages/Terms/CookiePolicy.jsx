da7c291fd1d830fa26d25fead2613e3b3c132194
Martine23FT <martinemidttveit@hotmail.com>
2024-10-11 02:00:26 +0200

    Changes

--- src/pages/Terms/CookiePolicy.jsx
+++ src/pages/Terms/CookiePolicy.jsx
@@ -1,15 +1,17 @@
 export default function CookiePolicy() {
-    <div className="bg-background">
-        <div className="px-[5%] md:px-[15%] lg:px-[25%] xl:px-[30%] 2xl:px-[36%] py-10 2xl:py-12">
-            <h2 className="text-xl xl:text-2xl font-bold pb-2">Cookie Policy</h2>
-            <ul className="list-disc pl-5 font-normal py-4 flex flex-col gap-2 pt-2">
-                <li>Holidaze uses cookies to enhance the user experience and gather data to improve our services.</li>
-                <li>By using our site, you consent to the use of cookies in accordance with this policy.</li>
-                <li>We use cookies to remember your preferences, analyze traffic, and understand usage patterns.</li>
-                <li>You can disable cookies through your browser settings, but doing so may affect your experience on the site.</li>
-            </ul>
+    return(
+        <div className="bg-background">
+            <div className="px-[5%] md:px-[15%] lg:px-[25%] xl:px-[30%] 2xl:px-[36%] py-10 2xl:py-12">
+                <h2 className="text-xl xl:text-2xl font-bold pb-2">Cookie Policy</h2>
+                <ul className="list-disc pl-5 font-normal py-4 flex flex-col gap-2 pt-2">
+                    <li>Holidaze uses cookies to enhance the user experience and gather data to improve our services.</li>
+                    <li>By using our site, you consent to the use of cookies in accordance with this policy.</li>
+                    <li>We use cookies to remember your preferences, analyze traffic, and understand usage patterns.</li>
+                    <li>You can disable cookies through your browser settings, but doing so may affect your experience on the site.</li>
+                </ul>
+            </div>
         </div>
-    </div>
+    )
 }
 
 
